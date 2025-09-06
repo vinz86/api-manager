019e335b557566a994ea732b3abc9881bcbb3ca1
vinz86 <vincenzo.disalvatore@gmail.com>
2025-09-06 20:20:18 +0200

    rimosso composable useFilters

--- composables/useFilters/useFilters.ts
+++
@@ -1,8 +0,0 @@
-import { useFilterBuilder } from './useFilterBuilder'
-import { useJhipsterFilterBuilder } from './useJhipsterFilterBuilder'
-
-export const useFilters = <T = Record<string, any>>(jhipsterMode = false) => {
-    return jhipsterMode
-        ? useJhipsterFilterBuilder<T>()
-        : useFilterBuilder<T>()
-}
